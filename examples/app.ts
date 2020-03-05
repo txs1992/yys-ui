b7f073fa7f2875d3a43205d92a4e2c729f745a04
陶旭生 <xusheng.txs@gmail.com>
2020-03-05 18:00:32 +0800

    feature：添加 typescript 与 webpack 等相关配置。

---
+++ examples/app.ts
@@ -0,0 +1,6 @@
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {
+  name: string = "MT";
+}
---
+++ examples/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">Welcome To Typescript, and your name is {{ name }}.</div>
+</template>
+
+<script lang="ts" src="./app.ts">
+</script>
+
+<style lang="scss">
+.app {
+  background: #ccc;
+}
+</style>
