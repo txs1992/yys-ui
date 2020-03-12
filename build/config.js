1e32e11507512ca163d29c822841f93aba6f0a6a
陶旭生 <xusheng.txs@gmail.com>
2020-03-12 16:26:00 +0800

    update .gitignore

--- build/config.js
+++ build/config.js
@@ -1,35 +1,66 @@
-import babel from "rollup-plugin-babel";
-import vue from "rollup-plugin-vue";
-import typescript from "rollup-plugin-typescript";
-import { terser } from "rollup-plugin-terser";
-import postcss from "rollup-plugin-postcss";
-import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
+const rollup = require("rollup");
+const vue = require("rollup-plugin-vue");
+const babel = require("rollup-plugin-babel");
+const postcss = require("rollup-plugin-postcss");
+const commonjs = require("rollup-plugin-commonjs");
+const resolve = require("rollup-plugin-node-resolve");
+const typescript = require("rollup-plugin-typescript");
+const terser = require("rollup-plugin-terser").terser;
 
-export default {
-  input: "packages/sidebar/src/index.vue",
-  // input: "test.js",
-  output: {
-    file: "dist/yys-ui.min.js",
-    // file: "bundle.js",
-    format: "iife",
-    name: "YysUI"
+const buildPlugins = [
+  resolve({
+    browser: true
+  }),
+  babel({
+    babelrc: true,
+    exclude: "node_modules/**"
+  }),
+  commonjs(),
+  typescript(),
+  vue(),
+  postcss({
+    extensions: [".css", ".scss"]
+  }),
+  terser()
+];
+
+const inputFileList = [
+  {
+    path: "packages/yys-ui/src/index.ts",
+    name: "YysUI",
+    bundleNmae: "yys-ui"
   },
-  external: ["vue"],
-  plugins: [
-    resolve({
-      browser: true
-    }),
-    babel({
-      babelrc: true,
-      exclude: "node_modules/**"
-    }),
-    commonjs(),
-    typescript(),
-    vue(),
-    // terser(),
-    postcss({
-      extensions: [".css", ".scss"]
-    })
-  ]
-};
+  {
+    path: "packages/sidebar/src/index.vue",
+    name: "YysSidebar",
+    bundleNmae: "sidebar"
+  }
+];
+
+async function build(inputOptions, outputOptions) {
+  const bundle = await rollup.rollup(inputOptions);
+  await bundle.write(outputOptions);
+}
+
+inputFileList.forEach(item => {
+  const inputOptions = {
+    input: item.path,
+    external: ["vue"],
+    plugins: buildPlugins.slice(0, 6)
+  };
+
+  const outputOptions = {
+    name: item.name,
+    file: `dist/${item.bundleNmae}.js`,
+    format: "umd"
+  };
+
+  const minInputOptions = { ...inputOptions, plugins: buildPlugins };
+  const minOutputOptions = {
+    ...outputOptions,
+    file: `dist/${item.bundleNmae}.min.js`
+  };
+
+  build(inputOptions, outputOptions);
+  build(minInputOptions, minOutputOptions);
+});
